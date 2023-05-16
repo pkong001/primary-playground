cdbfec1f7587ad0900156c680e4d1123d4e79736
pkong001 <pkongdan01@gmail.com>
2023-05-16 08:26:01 +0700

    update

--- playsound.py
+++ playsound.py
@@ -5,7 +5,7 @@ import pygame
 # Initialize Pygame
 pygame.mixer.init()
 
-# Sound choice
+# Sound choice4
 s1 = './sound/ping.mp3'
 s2 = './sound/click.mp3'
 
@@ -14,7 +14,7 @@ sound = pygame.mixer.Sound(s2)
 
 while True:
     # Generate a random number between 1 and 60
-    random_number = random.randint(30, 60)
+    random_number = random.randint(40, 60)
 
     # Wait for the number of seconds equal to the random number
     print(f"Waiting for {random_number} seconds...")
